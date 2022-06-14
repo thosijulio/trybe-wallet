e03eeeb9e18f95afd256b4beb22fa3c90c12398b
thosijulio <thosijulio@gmail.com>
2022-06-14 13:53:25 -0300

    feat-configurando-rota-para-o-componente

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AddExpense from '../components/AddExpense';
+import TableExpenses from '../components/TableExpenses';
 import WalletHeader from '../components/WalletHeader';
 
 class Wallet extends React.Component {
@@ -9,6 +10,7 @@ class Wallet extends React.Component {
       <>
         <WalletHeader />
         <AddExpense />
+        <TableExpenses />
       </>
     )
   }
