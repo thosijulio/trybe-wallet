cd15d337d00a7c05daa6a9fc04abc9fd39dac970
thosijulio <thosijulio@gmail.com>
2022-06-15 09:18:37 -0300

    feat-criando-acoes-ao-editar-excluir-despesa

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,7 @@
 export const CHANGE_EMAIL = 'CHANGE_EMAIL';
 export const ADD_EXPENSES = 'ADD_EXPENSES';
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
+export const REMOVE_EXPENSE = 'REMOVE_EXPENSE';
 
 export const changeEmailAction = (payload) => ({
   type: CHANGE_EMAIL,
@@ -8,5 +10,15 @@ export const changeEmailAction = (payload) => ({
 
 export const addExpensesAction = (payload) => ({
   type: ADD_EXPENSES,
+  payload,
+});
+
+export const editExpenseAction = (payload) => ({
+  type: EDIT_EXPENSE,
+  payload,
+});
+
+export const removeExpenseAction = (payload) => ({
+  type: REMOVE_EXPENSE,
   payload,
 });
--- src/redux/reducers/walletReducer.js
+++ src/redux/reducers/walletReducer.js
@@ -1,4 +1,4 @@
-import { ADD_EXPENSES } from "../actions";
+import { ADD_EXPENSES, EDIT_EXPENSE, REMOVE_EXPENSE } from "../actions";
 
 const INITIAL_STATE = {
   expenses: [],
@@ -12,6 +12,10 @@ const walletReducer = (state = INITIAL_STATE, action) => {
       return {
         expenses: payload,
       }
+    case EDIT_EXPENSE:
+      return state;
+    case REMOVE_EXPENSE:
+      return state;
     default:
       return state;
   }
